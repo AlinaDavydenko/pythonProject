3468d888710e85593e6e1fafd9b84ce5ba9658c9
Alina Davydenko <Alinadavydenko620@gmail.com>
2024-09-21 00:04:28 +0300

    f

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,17 +1,2 @@
-# import pytest
-import tempfile
-
-from src.decorators import log
-
-
-def test_log_good(capsys):
-    """Тестирует выполнение декорированной функции"""
-
-    @log(filename='mylog.txt')
-    def func(x, y):
-        return x + y
-
-    result = func(1, 2)
-    assert result == 3
 
 
